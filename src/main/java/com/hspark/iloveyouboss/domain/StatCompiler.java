653db5a8f0d709b9bdaf901acebccf8b7bb68136
sukeypark <hspark@data-alliance.com>
2020-12-30 10:19:50 +0900

    encapsulated method where controller is used

--- src/main/java/com/hspark/iloveyouboss/domain/StatCompiler.java
+++ src/main/java/com/hspark/iloveyouboss/domain/StatCompiler.java
@@ -23,18 +23,33 @@ public class StatCompiler {
 	private QuestionController controller = new QuestionController();
 	
 	public Map<String, Map<Boolean, AtomicInteger>> responsesByQuestion(
-			List<BooleanAnswer> answers) {
+			List<BooleanAnswer> answers, Map<Integer, String> questions) {
 		Map<Integer, Map<Boolean, AtomicInteger>> responses = new HashMap<>();
 		answers.stream().forEach(answer -> incrementHistogram(responses, answer));
-		return convertHistogramIdsToText(responses);
+		return convertHistogramIdsToText(responses, questions);
 	}
 
+	/*
+	 * 테스트: QuestionController의 find 메소드는 DB를 사용하기 때문에 빠른(Fast) 테스트를 할 수 없다.
+	 * - controller에서 question을 가져오는 부분을 캡슐화 한다(questionText).
+	 * 
+	 */
 	private Map<String, Map<Boolean, AtomicInteger>> convertHistogramIdsToText(
-			Map<Integer, Map<Boolean, AtomicInteger>> responses) {
+			Map<Integer, Map<Boolean, AtomicInteger>> responses, Map<Integer, String> questions) {
 		Map<String, Map<Boolean, AtomicInteger>> textResponses = new HashMap<>();
 		responses.keySet().stream().forEach(id ->
-				textResponses.put(controller.find(id).getText(), responses.get(id)));
+				textResponses.put(questions.get(id), responses.get(id)));
 		return textResponses;
+	}
+	
+	public Map<Integer, String> questionText(List<BooleanAnswer> answers) {
+		Map<Integer, String> questions = new HashMap<>();
+		answers.stream().forEach(answer -> {
+			if (!questions.containsKey(answer.getQuestionId())) {
+				questions.put(answer.getQuestionId(), controller.find(answer.getQuestionId()).getText());
+			}
+		});
+		return questions;
 	}
 
 	private void incrementHistogram(Map<Integer, Map<Boolean, AtomicInteger>> responses, BooleanAnswer answer) {
