2b7d415893bedca34b3e0ca12d55b7c00d46c04b
sukeypark <hspark@data-alliance.com>
2021-01-12 17:24:16 +0900

    주석위치 변경: 가독성 위해

--- src/main/java/com/hspark/iloveyouboss/domain/Profile.java
+++ src/main/java/com/hspark/iloveyouboss/domain/Profile.java
@@ -30,22 +30,22 @@ public class Profile {
 		
 		boolean kill = false;
 		boolean anyMatches = false;
+		/*
+		 * p.171 - 172
+		 * 리팩토링한 코드는 디메테르의 법칙(the Law of Demeter)를 위반하지 않음
+		 * 디메테르의 법칙(the Law of Demeter): 다른 객체로 전파되는 연쇄적인 메소드 호출을 피해야 함
+		 * 
+		 * answer 임시변수로 추출
+		 * 
+		 *  **임시변수의 활용**
+		 *  - 값 비싼 비용의 계산값을 캐시에 저장
+		 *  - 메소드 몸체에서 변경되는 것들을 수집
+		 *  - 코드의 의도를 명확하게 함
+		 */
+		
+		// answer 변수가 코드를 더 명확하게 해주지 않고 한번만 사용되므로 굳이 변수로 추출하지 않아도 된다.
+		// -> inline 코드로 변환
 		for (Criterion criterion: criteria) {
-			/*
-			 * p.171 - 172
-			 * 리팩토링한 코드는 디메테르의 법칙(the Law of Demeter)를 위반하지 않음
-			 * 디메테르의 법칙(the Law of Demeter): 다른 객체로 전파되는 연쇄적인 메소드 호출을 피해야 함
-			 * 
-			 * answer 임시변수로 추출
-			 * 
-			 *  **임시변수의 활용**
-			 *  - 값 비싼 비용의 계산값을 캐시에 저장
-			 *  - 메소드 몸체에서 변경되는 것들을 수집
-			 *  - 코드의 의도를 명확하게 함
-			 */
-			
-			// answer 변수가 코드를 더 명확하게 해주지 않고 한번만 사용되므로 굳이 변수로 추출하지 않아도 된다.
-			// -> inline 코드로 변환
 			boolean match = criterion.matches(answerMatching(criterion));
 			
 			if (!match && criterion.getWeight() == Weight.MustMatch) {
