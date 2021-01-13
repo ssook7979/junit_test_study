a280ea18e3057ad8c9f7785ca3030afaee365eba
sukeypark <hspark@data-alliance.com>
2021-01-13 14:38:08 +0900

    생성자에서 불필요한 값(score)를 생성하지 않고 호출시마다 생성하도록 refactoring

--- src/main/java/com/hspark/iloveyouboss/domain/MatchSet.java
+++ src/main/java/com/hspark/iloveyouboss/domain/MatchSet.java
@@ -7,26 +7,29 @@ import java.util.Map;
  */
 public class MatchSet {
 	private Map<String, Answer> answers;
-	private int score = 0;
 	private Criteria criteria;
 
 	public MatchSet(Map<String, Answer> answers, Criteria criteria) {
 		this.answers = answers;
 		this.criteria = criteria;
-		calculateScore(criteria);
-	}
-
-	private void calculateScore(Criteria criteria) {
-	   for (Criterion criterion: criteria) 
-	      if (criterion.matches(answerMatching(criterion))) 
-	         score += criterion.getWeight().getValue();
 	}
 	
 	private Answer answerMatching(Criterion criterion) {
 	   return answers.get(criterion.getAnswer().getQuestionText());
 	}
 	
+	/*
+	 * score는 상태변수로 둘 필요가 없으므로 필요할 때마다 계산할 수 있도록 한다.
+	 * 문제점
+	 * - 매번 계산하는 것은 성능저하를 일으키는가?
+	 * 	 => 지연 초기화(lazy initialization) 고려
+	 * 
+	 */
 	public int getScore() {
+		int score = 0;
+		for (Criterion criterion: criteria) 
+		   if (criterion.matches(answerMatching(criterion))) 
+		      score += criterion.getWeight().getValue();
 		return score;
 	}
 	
