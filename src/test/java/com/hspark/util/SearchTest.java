b0282b9f8dfc7b0eded7c7fa8bc18c57b0be2f5c
sukeypark <hspark@data-alliance.com>
2021-01-13 18:56:09 +0900

    테스트와 무관한 군더더기들은 BeforeEach, AfterEach 를 이용하여 테스트 코드로부터 분리한다.

--- src/test/java/com/hspark/util/SearchTest.java
+++ src/test/java/com/hspark/util/SearchTest.java
@@ -1,7 +1,6 @@
 package com.hspark.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static com.hspark.util.ContainsMatches.containsMatches;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -9,45 +8,56 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.logging.Level;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SearchTest {
 	// 추상화: 의미가 불분명한 '매직 리터럴' 대신 '상수'를 사용하여 가독성을 높인다.
 	private static final String ANY_TITLE = "1";
+	private InputStream stream;
+	
+	@BeforeEach
+	void turnOffLogging() {
+		Search.LOGGER.setLevel(Level.OFF);
+	}
+	
+	@AfterEach
+	void closeResources() throws Exception {
+		stream.close();
+	}
 	
 	@Test
 	public void testSearch() throws Exception {
-		// 테스트 데이터를 생성하는 부분을 추상화한다.
-		ByteArrayInputStream stream = streamOn("There are certain queer times and occasions "
+		stream = streamOn("There are certain queer times and occasions "
 				+ "in this strange mixed affair we call life when a man "
 				+ "takes this whole universe for a vast practical joke, "
 				+ "though the wit thereof he but dimly discerns, and more "
 				+ "than suspects that the joke is at nobody's expense but "
 				+ "his own.");
 		
-		Search search = new Search(stream, "practical joke", ANY_TITLE);
-		Search.LOGGER.setLevel(Level.OFF);
+		Search search = new Search(stream, "practical joke", ANY_TITLE);	
 		search.setSurroundingCharacterCount(10);
-		search.execute();
-		assertFalse(search.errored());
-		// 이 여러개의 assertions는 하나의 assertion으로 축약하여 가독성을 높일 수 있다.
+		search.execute();	
 		org.hamcrest.MatcherAssert.assertThat(search.getMatches(), containsMatches(new Match[] { 
 		         new Match(ANY_TITLE, "practical joke", 
-		                   "or a vast practical joke, though t") }));
-		stream.close();
+		                   "or a vast practical joke, though t") }));		
 	}
 	
-	// 여러개의 assertion은 다른 Test로 쪼갠다.
 	@Test
 	public void noMatchesReturnedWhenSearchStringNotInContent() throws Exception {
-		URLConnection connection = 
-				new URL("http://bit.ly/15sYPA7").openConnection();
-		InputStream inputStream = connection.getInputStream();
-		Search search = new Search(inputStream, "smelt", ANY_TITLE);
-		search.execute();
-		
+		URLConnection connection = new URL("http://bit.ly/15sYPA7").openConnection();
+		InputStream stream = connection.getInputStream();
+		Search search = new Search(stream, "smelt", ANY_TITLE);
+		search.execute();		
 		assertThat(search.getMatches()).isEmpty();
-		inputStream.close();
+	}
+	
+	// 이 assertion은 지금 당장 필요하지 않지만 추후에 다른 테스트에 사용해야 하므로 메모...
+	// TODO:
+	@Test
+	public void noErrorOccurred() {
+		// assertFalse(search.errored());
 	}
 
 	private ByteArrayInputStream streamOn(String pageContent) {
