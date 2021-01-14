eccc736d734191e1318d8a6d20adec28d8022e8e
sukeypark <hspark@data-alliance.com>
2021-01-14 10:12:23 +0900

    또 다른 실패하는 테스트 작성

--- src/test/java/com/hspark/tdd/ProfileTest.java
+++ src/test/java/com/hspark/tdd/ProfileTest.java
@@ -20,5 +20,18 @@ class ProfileTest {
 		
 		assertFalse(result);
 	}
+	
+	@Test
+	void matchesWhenProfileContainsMatchingAnswer() {
+		Profile profile = new Profile();
+		Question question = new BooleanQuestion(1, "Relocation package?");
+		Answer answer = new Answer(question, Bool.TRUE);
+		profile.add(answer);
+		Criterion criterion = new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare);
+		
+		boolean result = profile.matches(criterion);
+		
+		assertTrue(result);
+	}
 
 }
