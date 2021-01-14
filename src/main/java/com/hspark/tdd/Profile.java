0a5d05636b31752598243639b9da2aafd266de27
sukeypark <hspark@data-alliance.com>
2021-01-14 11:19:41 +0900

    Profile의 multiple answers 중에서 criterion의 answer와 매칭되는 것 찾는 로직 추가

--- src/main/java/com/hspark/tdd/Profile.java
+++ src/main/java/com/hspark/tdd/Profile.java
@@ -8,7 +8,13 @@ public class Profile {
 	private Map<String, Answer> answers = new HashMap<>();
 
 	public boolean matches(Criterion criterion) {
+		Answer answer = getMatchingPrfileAnswer(criterion);
 		return answer != null && answer.match(criterion.getAnswer());
+	}
+
+	private Answer getMatchingPrfileAnswer(Criterion criterion) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	public void add(Answer answer) {
