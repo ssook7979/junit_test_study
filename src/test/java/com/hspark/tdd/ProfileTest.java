d7a391338598ebd875b8ba379190e357a2f7fcb2
sukeypark <hspark@data-alliance.com>
2021-01-14 10:09:02 +0900

    시나리오에 따라 실패하는 테스트 작성

--- src/test/java/com/hspark/tdd/ProfileTest.java
+++ src/test/java/com/hspark/tdd/ProfileTest.java
@@ -1,14 +1,24 @@
 package com.hspark.tdd;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
 class ProfileTest {
-
+	/*
+	 * 시나리오에 따라 실패하는 테스트 작성
+	 * 1. BooleanQuestion 타입의 Question을 Criterion에 등록
+	 * 2. Profile에 기준에 맞는 Answer이 없는 경우 False 반환
+	 */
 	@Test
-	void test() {
-		new Profile();
+	void matchesNothingWhenProfileEmpty() {
+		Profile profile = new Profile();
+		Question question = new BooleanQuestion(1, "Relocation package?");
+		Criterion criterion = new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare);
+		
+		boolean result = profile.matches(criterion);
+		
+		assertFalse(result);
 	}
 
 }
