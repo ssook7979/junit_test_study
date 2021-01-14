e7b0ef19cad7ef56eef85159a1aaffeb498cb6c2
sukeypark <hspark@data-alliance.com>
2021-01-14 09:59:53 +0900

    Test 생성

---
+++ src/test/java/com/hspark/tdd/ProfileTest.java
@@ -0,0 +1,14 @@
+package com.hspark.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProfileTest {
+
+	@Test
+	void test() {
+		new Profile();
+	}
+
+}
